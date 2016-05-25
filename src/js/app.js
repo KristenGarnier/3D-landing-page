ad24779938f4cff2af46bbda63291fc4bb118a99
KristenGarnier <garnier.kristen@icloud.com>
2016-05-25 10:22:26 +0200

    Corrections et optimisation des images

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
+var smoothScroll = require('smooth-scroll');
+
 document.querySelector('#contact').addEventListener('click', function (e) {
   document.querySelector('#contact iframe').style.pointerEvents = 'auto';
 }, false);
+
+smoothScroll.init();
